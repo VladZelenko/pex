8598e51c78b78b9022ee50a996a95edfa5eccf1b
VladZelenko <Vlad6555@ukr.net>
2017-05-03 16:19:11 +0300

    add new structure project and new nav menu

---
+++ app/js/common.js
@@ -0,0 +1,18 @@
+//mobile menu
+function openNav(){
+	document.getElementById('nav').style.width = "100%";
+	document.getElementById('open-btn-nav').style.opacity = "0";
+}
+function closeNav(){
+	document.getElementById('nav').style.width = "0%";
+	document.getElementById('open-btn-nav').style.opacity = "1";
+}
+//випадашка по вертикалі, працює при закоменченому по горизонталі
+// function openNav(){
+// 	document.getElementById('nav').style.height = "100%";
+//	document.getElementById('open-btn-nav').style.opacity = "0";
+// }
+// function closeNav(){
+// 	document.getElementById('nav').style.height = "0%";
+//	document.getElementById('open-btn-nav').style.opacity = "1";
+// }
